f7350cd8fc65caba143f6e6e28ff878764d748e2
huangjingwen <huangjingwen@zhihu.com>
2023-11-18 17:36:04 +0800

    Add ### Saving Models ### and ### Loading Models ###

--- quickstart.py
+++ quickstart.py
@@ -283,3 +283,33 @@ print("Done!")
 #  Accuracy: 67.0%, Avg loss: 0.982054
 # 
 # Done!
+
+### Saving Models ###
+
+torch.save(model.state_dict(), "model.pth")
+print("Saved PyTorch Model State to model.pth")
+
+### Loading Models ###
+
+model = NeuralNetwork().to(device)
+model.load_state_dict(torch.load("model.pth"))
+
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
+
+model.eval()
+x, y = test_data[0][0], test_data[0][1]
+with torch.no_grad():
+    logits = model(x)
+    predicted, actual = classes[logits[0].argmax(0)], classes[y]
+    print(f'Predicted: "{predicted}", Actual: "{actual}"')
